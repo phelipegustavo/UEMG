2a3210757dc5381df6169370631e56b214d869f2
Phelipe Silva <Phelipediv@gmail.com>
2017-04-11 16:03:13 -0300

    Lista 03 - POO

--- 4 Periodo/POO/Lista 03/Exercicio01/src/exercicio01/Curso.java
+++ 4 Periodo/POO/Lista 03/Exercicio01/src/exercicio01/Curso.java
@@ -30,9 +30,9 @@ public class Curso {
     public void excluiTurma(int posicao) {
         if (turmas[posicao] != null) {
             turmas[posicao] = null;
-            System.out.println("Turma inserida com sucesso");
+            System.out.println("Turma excluida com sucesso");
         } else {
-            System.out.println("Impossível inserir turma");
+            System.out.println("Impossível excluida turma");
         }
     }
     
