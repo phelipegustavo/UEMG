6121776e44622d17d3e2b2a92494df06a9619b4f
Phelipe Silva <Phelipediv@gmail.com>
2017-05-29 13:21:56 -0300

    Aula006 POO

---
+++ 4 Periodo/POO/Aula006/src/aula006/Aula006.java
@@ -0,0 +1,20 @@
+package aula006;
+
+/**
+ *
+ * @author phelipe
+ */
+public class Aula006 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        MinhaPrimeiraTela tela = new MinhaPrimeiraTela();
+        
+        tela.configurarJanela();
+        
+    }
+    
+}
