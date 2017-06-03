383a9d2c725b0b06f1f480e0d0e5489cd5f98ea7
Phelipe Silva <phelipediv@gmail.com>
2017-06-03 11:37:56 -0300

    Lista 07 POO

---
+++ 4 Periodo/POO/Lista 07/Exercicio 01/src/pkg/Exercicio01.java
@@ -0,0 +1,18 @@
+package pkg;
+
+/**
+ *
+ * @author phelipe
+ */
+public class Exercicio01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        MediaJFrame mediaJFrame = new MediaJFrame();
+        mediaJFrame.setVisible(true);
+    }
+    
+}
---
+++ 4 Periodo/POO/Lista 07/Exercicio 02/src/exercicio/pkg02/Exercicio02.java
@@ -0,0 +1,18 @@
+package exercicio.pkg02;
+
+/**
+ *
+ * @author phelipe
+ */
+public class Exercicio02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        JurosJFrame jFrame = new JurosJFrame();
+        jFrame.setVisible(true);
+    }
+    
+}
